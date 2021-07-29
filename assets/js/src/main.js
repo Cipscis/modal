f7f7f00a8602362533a6d149816d169d5fd48734
Cipscis <cipscis+github@gmail.com>
2021-07-29 00:52:52 +0000

    Deploying to gh-pages from @ Cipscis/modal@32a63ce265562ce8dfecef313de7de739dba10ce 🚀

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -3,4 +3,22 @@
 // even though it is the root for the server
 // both locally and on GitHub Pages
 
-import '/modal.js';
+import * as modal from '/modal.js';
+
+modal.init({
+	onShow: console.log,
+});
+
+const selectors = Object.freeze({
+	show: '.js-example__show',
+	hide: '.js-example__hide',
+});
+
+const showExample = (e) => {
+	e.stopPropagation();
+
+	modal.show('modal-show-example');
+};
+document.querySelectorAll(selectors.show).forEach(($el) => $el.addEventListener('click', showExample));
+
+document.querySelectorAll(selectors.hide).forEach(($el) => $el.addEventListener('click', modal.hide));
